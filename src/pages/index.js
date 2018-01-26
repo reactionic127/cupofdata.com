61ecb55debcd64715e16f8c5f0b26def67cdd680
GoolzziJeon <goolzzijeon@gmail.com>
2018-01-26 23:57:14 +0800

    Service of COD section

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,43 +2,47 @@ import React from 'react'
 import { Container, Card, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap'
 import Link, { withPrefix} from 'gatsby-link'
 import graphql from 'graphql'
+import SliderCODView from '../containers/SliderCODView'
 
 import '../sass/landing.sass';
 
 const IndexPage = ({ data }) => (
-  <div className="banner-wrapper">
-    <Container>
-      <div className="navbar navbar-expand-lg">
-        <Link to="/" className="navbar-brand">
-          <img className="logo" src={withPrefix('/files/images/img-logo.svg')}></img>
-        </Link>
-        <ul className='nav navbar-nav'>
-          <li className='nav-item'>
-            <Link to='/features' className='nav-link'>Features</Link>
-          </li>
-          <li className='nav-item'>
-            <Link to='/pricing' className='nav-link'>Pricing</Link>
-          </li>
-          <li className='nav-item'>
-            <Link to='/company' className='nav-link'>Company</Link>
-          </li>
-          <li className='nav-item'>
-            <Link to='/resouces' className='nav-link'>Resources</Link>
-          </li>
-          <li className='nav-item'>
-            <Link to='/blog' className='nav-link'>Blog</Link>
-          </li>
-          <li className='nav-item'>
-            <Link to='/login' className='nav-link'>Login</Link>
-          </li>
-        </ul>
-      </div>
-      <div className="banner-view pa-5">
-        <h1>Sip of your future</h1>
-        <p>Duis aute irure dolor in reprehenderit in voluptate velid esse cillum dolore eu fugiat nulla pariatur</p>
-        <button className="submit-button">Regiser Now</button>
-      </div>
-    </Container>
+  <div>
+    <div className="banner-wrapper">
+      <Container>
+        <div className="navbar navbar-expand-lg">
+          <Link to="/" className="navbar-brand">
+            <img className="logo" src={withPrefix('/files/images/img-logo.svg')}></img>
+          </Link>
+          <ul className='nav navbar-nav'>
+            <li className='nav-item'>
+              <Link to='/features' className='nav-link'>Features</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/pricing' className='nav-link'>Pricing</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/company' className='nav-link'>Company</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/resouces' className='nav-link'>Resources</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/blog' className='nav-link'>Blog</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/login' className='nav-link'>Login</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="banner-view pa-5">
+          <h1>Sip of your future</h1>
+          <p>Duis aute irure dolor in reprehenderit in voluptate velid esse cillum dolore eu fugiat nulla pariatur</p>
+          <button className="submit-button">Regiser Now</button>
+        </div>
+      </Container>
+    </div>
+    <SliderCODView />
   </div>
 )
 
