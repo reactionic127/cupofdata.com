6cfe762f6cb4db261e343b4a21ceb0a548bd0393
GoolzziJeon <goolzzijeon@gmail.com>
2018-01-27 10:37:06 +0800

    Add layout

---
+++ src/layouts/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { withPrefix } from 'gatsby-link'
+import { Container } from 'reactstrap'
+import PropTypes from 'prop-types'
+import graphql from 'graphql'
+import Link from 'gatsby-link'
+import Helmet from 'react-helmet'
+
+// code syntax-highlighting theme
+// feel free to change it to another one
+import 'prismjs/themes/prism-twilight.css'
+
+// main site style
+import './index.scss'
+
+
+const TemplateWrapper = ({ children, data }) => {
+  let user
+  if (typeof window !== 'undefined') {
+    user = window.netlifyIdentity && window.netlifyIdentity.currentUser()
+  }
+  return (
+    <div className='App'>      
+      <div className='pageContent'>{children()}</div>
+    </div>
+  )
+}
+
+TemplateWrapper.propTypes = {
+  children: PropTypes.func
+}
+
+export const pageQuery = graphql`
+  query LayoutIndexQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
+
+export default TemplateWrapper
