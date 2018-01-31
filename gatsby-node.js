f1a30438bc517c76c85dcba98945ca7050241dd8
Goolzzi <goolzzijeon@gmail.com>
2018-01-31 00:44:31 -0800

    add sass module

--- gatsby-node.js
+++ gatsby-node.js
@@ -34,3 +34,74 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
     })
   })
 }
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const { cssModulesConfig } = require('gatsby-1-config-css-modules');
+
+exports.modifyWebpackConfig = ({ config, stage }) => {
+  const sassFiles = /\.s[ac]ss$/;
+  const sassModulesFiles = /\.module\.s[ac]ss$/;
+  const sassLoader = `sass?${JSON.stringify({
+    includePaths: path.resolve(path.resolve(), '../open/src/'),
+  })}`;
+
+  const extractText = new ExtractTextPlugin('styles.css', { allChunks: true });
+
+  switch (stage) {
+    case 'develop': {
+      config.loader('sass', {
+        test: sassFiles,
+        exclude: sassModulesFiles,
+        loaders: ['style', 'css', sassLoader],
+      });
+
+      config.loader('sassModules', {
+        test: sassModulesFiles,
+        loaders: ['style', cssModulesConfig(stage), sassLoader],
+      });
+
+      return config;
+    }
+    case 'build-css': {
+      config.loader('sass', {
+        test: sassFiles,
+        exclude: sassModulesFiles,
+        loader: ExtractTextPlugin.extract(['css?minimize', sassLoader]),
+      });
+
+      config.loader('sassModules', {
+        test: sassModulesFiles,
+        loader: ExtractTextPlugin.extract('style', [
+          cssModulesConfig(stage),
+          sassLoader,
+        ]),
+      });
+
+      config._config.plugins.splice(1, 1, extractText);
+
+      return config;
+    }
+    case 'develop-html':
+    case 'build-html':
+    case 'build-javascript': {
+      config.loader('sass', {
+        test: sassFiles,
+        exclude: sassModulesFiles,
+        loader: 'null',
+      });
+
+      config.loader('sassModules', {
+        test: sassModulesFiles,
+        loader: ExtractTextPlugin.extract('style', [
+          cssModulesConfig(stage),
+          sassLoader,
+        ]),
+      });
+
+      return config;
+    }
+    default: {
+      return config;
+    }
+  }
+};
