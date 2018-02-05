6553b7c9646b2a978c4a285a08eb2bb447cb4fad
Goolzzi <goolzzijeon@gmail.com>
2018-02-05 08:01:36 -0800

    bug: Sort of shutters appeared often - Issue #34
    avoid theme color

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const Button = styled.button`
 	padding: 0.7rem 5rem;
   border-radius: 1.5rem;
-  background-color: ${props => props.theme.color.orange};
+  background-color: #ff842b;
   border: none;
   color: white;
   font-family: CircularStd;
