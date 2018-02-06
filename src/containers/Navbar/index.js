02a7a014b8d25298efb8ae6b0b917ad7074102be
Goolzzi <goolzzijeon@gmail.com>
2018-02-06 09:20:55 -0800

    Add hamburger button

--- src/containers/Navbar/index.js
+++ src/containers/Navbar/index.js
@@ -24,7 +24,6 @@ const NavWrapper =  styled.ul`
   flex-direction: row;
   list-style: none;
   align-items: center;
-  flex-wrap: wrap;
 `
 const NavLink = styled(Link)`
   padding: 0.5rem 1.75rem;
@@ -36,12 +35,74 @@ const NavSection = Container.extend`
   justify-content: space-around;
 `
 
+const MobileNav = styled.div`
+  display: none;
+  position: relative;
+  background: transparent;
+  @media only screen and (max-width: 768px) {
+    display: block;
+  }
+`
+const Toggle = styled.a`
+  position: absolute;
+  left: 18px;
+  top: 22px;
+  cursor: pointer;
+  padding: 10px 35px 16px 0px;
+`
+const ToggleSpan = styled.span`
+  cursor: pointer;
+  border-radius: 1px;
+  height: 5px;
+  width: 35px;
+  background: #ffffff;
+  position: absolute;
+  display: block;
+  content: '';
+  transition: all 300ms ease-in-out;
+  ::before {
+    cursor: pointer;
+    border-radius: 1px;
+    height: 5px;
+    width: 35px;
+    background: #ffffff;
+    position: absolute;
+    display: block;
+    content: '';
+    top: -10px;
+    transition: all 300ms ease-in-out;
+  }
+  ::after {
+    cursor: pointer;
+    border-radius: 1px;
+    height: 5px;
+    width: 35px;
+    background: #ffffff;
+    position: absolute;
+    display: block;
+    content: '';
+    transition: all 300ms ease-in-out;
+    bottom: -10px;
+  }
+  ${props => props.active && css`
+    background-color: transparent;
+    ::before {
+      top: 0;
+      transform: rotate(45deg);
+    }
+    ::before {
+      top: 0;
+      transform: rotate(-45deg);
+    }
+  `}
+`
 const NavContainer = (props) => (
   <Navbar>
     <NavSection>
       <Link to="/" className="navbar-brand">
         <img className="logo" src={withPrefix('/files/images/img-logo.png')} width="120" />
       </Link>
+      <MobileNav><Toggle><ToggleSpan></ToggleSpan></Toggle></MobileNav>
       <NavWrapper>
         <NavItem>
           <NavLink to='/features'>Features</NavLink>
