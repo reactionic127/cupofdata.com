64a024d2234766c466b0d269e37dab4822d88501
Goolzzi <goolzzijeon@gmail.com>
2018-02-06 17:44:39 -0800

    feature: Add master blog page - Issue #9
    Signed-off-by: Goolzzi <goolzzijeon@gmail.com>

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,25 +2,85 @@ import React, { Fragment } from 'react'
 import styled from 'styled-components'
 import Helmet from 'react-helmet'
 import graphql from 'graphql'
-import { Container } from '../components/Layout'
+import { Container, Row, Col } from '../components/Layout'
+import Button from '../components/Button'
 
 const MainSection = styled.div`
 	padding-top: 94px;
   height: fit-content;
 `
-
+const Card = styled.div`
+	padding: 2rem;
+	border: 1px solid rgba(0,0,0, 0.1);
+	max-width: 780px;
+	margin: 1rem auto;
+	height: 280px;
+`
+const BlogContainer = Container.extend`
+	height: 100%;
+	box-sizing: border-box;
+`
+const BlogRow = Row.extend`
+	height: 100%;
+	align-items: center;
+`
+const BlogImage= styled.img`
+	border: 2px #35a73e solid;
+`
+const Topic = styled.h2`
+	font-size: 24px;
+	line-height: 31px;
+	font-family: GTWalsheim;
+`
+const TimeStamp = styled.h5`
+	font-family: Avenir;
+	font-size: 16px;
+	line-height: 24px;
+	color: rgba(0,0,0,0.2);
+`
+const Summary = styled.p`
+	font-family: Avenir;
+	font-size: 16px;
+	line-height: 26px;
+	color: #000000;
+	text-overflow: ellipsis;
+  height: 130px;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 5;
+  -webkit-box-orient: vertical;
+`
+const LoadBtn = Button.extend`
+	display: block;
+	margin: 50px auto 73px;
+`
 const BlogMaster = ({data}) => {
 	const { allMarkdownRemark: post } = data
 	const blog = post.edges.filter((post) => post.node.frontmatter.contentType === 'blog')
-  console.log('-- blogmaster remark --\n', blog)
 	return (
 		<Fragment>
 	    <Helmet title={`Blogs | ${data.site.siteMetadata.title}`} />
-
 	    <MainSection>
-	      <Container>
-	        
-	      </Container>
+	      { blog && <Container>
+	      	{ blog.map(({ node: post }, i) => (
+	      		<Card key={i}>
+	      			<BlogContainer>
+	      				<BlogRow>
+	      					<Col md="7">
+	      						<Topic>{post.frontmatter.title}</Topic>
+	      						<TimeStamp>{post.frontmatter.author} on {post.frontmatter.date}</TimeStamp>
+	      						<Summary>{post.frontmatter.summary}</Summary>
+	      					</Col>
+	      					<Col md="5">
+	      						<BlogImage src={post.frontmatter.postimage} width="100%"/>
+	      					</Col>
+	      				</BlogRow>
+	      			</BlogContainer>
+	      		</Card>
+	      	))}
+	      	<LoadBtn outline>Load more</LoadBtn>
+	      	</Container>
+	      }
 	    </MainSection>
 	  </Fragment>
 	 )
@@ -39,6 +99,9 @@ export const blogMasterQuery = graphql`
             contentType
             date(formatString: "MMMM DD, YYYY")
             path
+            summary
+            author
+            postimage
           }
         }
       }
