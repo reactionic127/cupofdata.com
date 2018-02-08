43937a1e12f9bdf478a7353f9abdd3d29d87d795
Goolzzi <goolzzijeon@gmail.com>
2018-02-07 18:34:59 -0800

    testminerals position
    Signed-off-by: Goolzzi <goolzzijeon@gmail.com>

--- src/containers/Feedback/index.js
+++ src/containers/Feedback/index.js
@@ -13,14 +13,17 @@ const Wrapper = styled.div`
 const FeedbackSectionTitle = SectionTitle.extend`
 	margin-bottom: 70px
 `
-
+const TestimonialsRow = Row.extend`
+	justify-content: center;
+`
 const FeedbackSection = () => (
 	<Wrapper>
 		<FeedbackSectionTitle>Testimonials</FeedbackSectionTitle>
 		<Container>
-			<Row>
+			<TestimonialsRow>
 				<Col xs="4">
 					<AvatarCard
+						avatarUrl="/files/images/avatar-robert.png"
 						name="Robert Joseph"
 						position="Data Scientist, Stanley Black and Decker"
 						quote="The easy to use, robust API made it simple to integrate 3Blades quickly and safely with internal applications."
@@ -28,12 +31,13 @@ const FeedbackSection = () => (
 				</Col>
 				<Col xs="4" className="text-center">
 					<AvatarCard
+						avatarUrl="/files/images/avatar-anna.png"
 						name="Anna Anisin"
 						position="Founder, Formulated.by"
 						quote="We obtained higher quality B2B leads, faster, and were able to integrate with the Martech tools we know and love."
 					/>
 				</Col>
-			</Row>
+			</TestimonialsRow>
 		</Container>
 	</Wrapper>
 )
