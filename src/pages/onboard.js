e3811f1d8db9f06b75ae0eb1e54279938518c8af
Goolzzi <goolzzijeon@gmail.com>
2018-02-08 00:15:40 -0800

    feature: Add lead capture bot - Issue #52
    Signed-off-by: Goolzzi <goolzzijeon@gmail.com>

---
+++ src/pages/onboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Iframe = styled.iframe`
+	position: absolute;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+`
+const Onboarding = () => (
+	<Iframe width="100%" height="100%" frameborder="0" src="https://landbot.io/u/H-25231-G88YW0YD97JZI1KT/index.html"></Iframe>
+)
+export default Onboarding
