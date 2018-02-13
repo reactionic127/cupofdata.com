70d84d36789bc0b17e92e9cb773c9b7af5bec981
Goolzzi <goolzzijeon@gmail.com>
2018-02-13 00:15:06 -0800

    bug: Legal docs do not have the correct design - Issue #82
    Signed-off-by: Goolzzi <goolzzijeon@gmail.com>

--- src/templates/legal.js
+++ src/templates/legal.js
@@ -1,17 +1,41 @@
 import React from 'react'
-import { Container } from '../components/Layout'
+import styled from 'styled-components'
 import Helmet from 'react-helmet'
 import graphql from 'graphql'
 
+import { Container } from '../components/Layout'
+import { H1, H6 } from '../components/Typography'
+
+const Wrapper = styled.div`
+  padding: 90px 0;
+  background: #f7f5fa;
+  text-align: center;
+`
+const Title = H1.extend`
+  margin-top: 91px;
+`
+const Summary = H6.extend`
+  margin: 10px auto;
+  font-weight: 400;
+`
+const ContentWrapper = styled.div`
+  padding: 50px 0;
+`
 export default function Template ({ data }) {
   const { markdownRemark: post } = data
+  console.log('-- post --', post.frontmatter)
   return (
     <div>
       <Helmet title={`${post.frontmatter.title} | ${data.site.siteMetadata.title}`} />
-      <Container>
-        <h1 className='display-3'>{post.frontmatter.title}</h1>
-      </Container>
-      <Container dangerouslySetInnerHTML={{ __html: post.html }} />
+      <Wrapper>
+        <Container>
+          <Title>{post.frontmatter.title}</Title>
+          <Summary>{post.frontmatter.summary}</Summary>
+        </Container>
+      </Wrapper>
+      <ContentWrapper>
+        <Container className="cms-content" dangerouslySetInnerHTML={{ __html: post.html }} />
+      </ContentWrapper>
     </div>
   )
 }
@@ -23,6 +47,7 @@ export const legalPageQuery = graphql`
       frontmatter {
         path
         title
+        summary
       }
     }
     site {
