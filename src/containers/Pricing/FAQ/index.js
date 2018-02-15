af5b8191957bb96de8071d5df3bcc33e9f87869a
Goolzzi <goolzzijeon@gmail.com>
2018-02-14 16:51:58 -0800

    udpate FAQ
    Signed-off-by: Goolzzi <goolzzijeon@gmail.com>

--- src/containers/Pricing/FAQ/index.js
+++ src/containers/Pricing/FAQ/index.js
@@ -52,36 +52,28 @@ const FaqSection = () => (
 		<Title>Frequently Asked Questions</Title>
 		<Content>
 			<QASection>
-				<Question>What is the maximum number virtual servers allowed?</Question>
-				<Answer>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </Answer>
+				<Question>What forms of payment do you accept?</Question>
+				<Answer>Thanks to the good people at Stripe, we accept all major credit cards. We also accept wire transfers and electronic checks.</Answer>
 			</QASection>
 			<QASection>
-				<Question>Do you accept paypal?</Question>
-				<Answer>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Answer>
+				<Question>How is your service different from all other B2B lead gen service out there?</Question>
+				<Answer>Cup of Data uses Machine Learning and Deep Learning to identify the best leads, for you. Our A.I. calculates an Account Propensity Score (APS) which improves over time by tracking successful conversions. So it's more than just B2B lead gen, it's your own personal an automated sales coach!</Answer>
 			</QASection>
 			<QASection>
-				<Question>Is the amount refundable?</Question>
-				<Answer>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </Answer>
+				<Question>Do you leverage third-party databases?</Question>
+				<Answer>Absolutely. We rely on firmographic, technographic, social, and public data and in many cases, these data sources have costs and long-term commitments.</Answer>
 			</QASection>
 			<QASection>
-				<Question>Lorem ipsum dolor sit amet consectetur adipiscing?</Question>
-				<Answer>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </Answer>
+				<Question>Do you integrate with CRM and Martech solutions?</Question>
+				<Answer>You bet. We designed Cup of Data from the get-go to integrate with any solution.</Answer>
 			</QASection>
 			<QASection>
-				<Question>Do you accept paypal?</Question>
-				<Answer>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Answer>
+				<Question>Do you offer free trials for your service?</Question>
+				<Answer>We do for a limited number of accounts and validated contacts. Cup of Data works best once our A.I. models track milestones, starting with conversions, and create challenger models to improve outcomes. If you would like a demo, you can schedule one <a href="https://www.cupofdata.com/onboard/">here</a></Answer>
 			</QASection>
 			<QASection>
-				<Question>What is the maximum number virtual servers allowed?</Question>
-				<Answer>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </Answer>
-			</QASection>
-			<QASection>
-				<Question>Lorem ipsum dolor sit amet consectetur adipiscing?</Question>
-				<Answer>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </Answer>
-			</QASection>
-			<QASection>
-				<Question>Is the amount refundable?</Question>
-				<Answer>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</Answer>
+				<Question>Do you augment account and contact lists with context?</Question>
+				<Answer>Yes. If you can engage the customer with the right message, at the right time, and in the right format you will have a big advantage. Context helps accomplish that. </Answer>
 			</QASection>
 		</Content>
 	</Wrapper>
