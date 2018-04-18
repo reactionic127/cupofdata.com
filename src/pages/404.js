7886475bf5e4815f12dea465d718204f8f309151
reactionic127 <richard.gill127@gmail.com>
2018-04-18 20:42:15 +0100

    404 style is fixed

--- src/pages/404.js
+++ src/pages/404.js
@@ -19,16 +19,6 @@ const Wrapper = styled.div`
   align-items: center;
 `
 
-const LogoImg = styled.img`
-  margin-top: 80px;
-  width: 170px;
-`
-
-const Img404 = styled.img`
-  margin-top: 100px;
-  width: 430px;
-`
-
 const Topic = styled.h1`
   font-size: 30px;
   line-height: 41px;
@@ -51,12 +41,33 @@ const BackButton = Button.extend`
   margin-top: 3rem;
   box-shadow: 2px 4px rgba(0, 0, 0, 0.1);
 `
+const logoStyle = {
+  marginTop: '80px',
+}
+
+const imgStyle = {
+  marginTop: '100px',
+}
 
 const NotFoundPage = () => (
   <Wrapper>
     <Helmet title="Page Not Found | Cup of Data" />
-    <LogoImg src={imgLogo} />
-    <Img404 src={img404} />
+    {typeof window !== 'undefined' && (
+      <div>
+        <LazyLoadImage
+          effect="blur"
+          src={imgLogo}
+          width="170px"
+          style={logoStyle}
+        />
+        <LazyLoadImage
+          effect="blur"
+          src={img404}
+          width="430px"
+          style={imgStyle}
+        />
+      </div>
+    )}
     <Topic>Page not found</Topic>
     <Detail>
       Sorry, but the page you were looking for could not be found. You can
